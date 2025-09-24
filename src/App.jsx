e499a871dac0282ab59e539d44430f9c6a8e3a10
gueon7866 <lsg7866@gmail.com>
2025-09-24 19:39:31 +0900

    `
    1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header'
+import main from './components/main.jsx';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header />
+    <Main />
+     
+    </>
+  )
+}
+
+export default App
